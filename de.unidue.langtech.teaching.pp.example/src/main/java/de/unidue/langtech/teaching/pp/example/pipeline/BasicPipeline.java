d42e44853da99466197306cc804a217571f030df
Marius <Marius@D41-5H0.local>
2015-12-11 08:19:25 +0100

    Updated to Tutorial Stage1

--- de.unidue.langtech.teaching.pp.example/src/main/java/de/unidue/langtech/teaching/pp/example/pipeline/BasicPipeline.java
+++ de.unidue.langtech.teaching.pp.example/src/main/java/de/unidue/langtech/teaching/pp/example/pipeline/BasicPipeline.java
@@ -4,6 +4,7 @@ import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 
+import de.tudarmstadt.ukp.dkpro.core.snowball.SnowballStemmer;
 import de.unidue.langtech.teaching.pp.example.BaselineExample;
 import de.unidue.langtech.teaching.pp.example.EvaluatorExample;
 import de.unidue.langtech.teaching.pp.example.ReaderExample;
@@ -20,7 +21,8 @@ public class BasicPipeline
                         ReaderExample.PARAM_INPUT_FILE, "src/test/resources/test/input.txt"
                 ),
                 AnalysisEngineFactory.createEngineDescription(BaselineExample.class),
-                AnalysisEngineFactory.createEngineDescription(EvaluatorExample.class)
-        );
+                AnalysisEngineFactory.createEngineDescription(EvaluatorExample.class),
+                AnalysisEngineFactory.createEngineDescription(SnowballStemmer.class)
+        );                
     }
 }
